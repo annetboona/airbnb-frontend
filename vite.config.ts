56b6606575c2928783f6bbc84ed572ad2dfa5499
Denyse-BAYINGANA-UMUHOZA <denysebayingana1@gmail.com>
2026-05-15 10:51:10 +0200

    deployment url

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/react-vite-deployment",
   server: {
     proxy: {
       // Any request to /api/v1/* is forwarded to the deployed Render backend
       '/api/v1': {
         target: 'https://airbnb-xr0i.onrender.com',
-        changeOrigin: true,   // rewrites the Host header → no CORS block
+        changeOrigin: true,   
         secure: true,
       },
     },
